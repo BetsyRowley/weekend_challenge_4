6625c66e0977511f6767a79db0e3a8cd4e7a65dc
Betsy Rowley <BRowley8@gmail.com>
2017-03-31 16:46:21 -0500

    Sets up Grunt file.

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON("package.json"),
+    copy: {
+      jquery: {
+        expand: true,
+        cwd: "node_modules/jquery/dist/",
+        src: ["jquery.min.js"],
+        dest: "server/public/vendors/"
+      },
+      bootstrap: {
+        expand: true,
+        cwd: "node_modules/bootstrap/dist",
+        src:["*"],
+        dest: "server/public/vendors/"
+      }
+    },
+  });
+
+
+grunt.loadNpmTasks("grunt-contrib-copy");
+
+grunt.registerTask("default", ["copy"]);
+
+};
