2f57271de52f7541ed281d7aa8abad55c1fb411f
Betsy Rowley <BRowley8@gmail.com>
2017-03-31 16:31:37 -0500

    Starts server and connects MongoDB

--- server/app.js
+++ server/app.js
@@ -6,12 +6,23 @@ var bodyParser = require("body-parser");
 ///add route require & respective app.use
 
 var mongoose = require("mongoose");
-var mongoURI = "mongodb://localhost27017/employees"; //reflect actual db
+var mongoURI = "mongodb://localhost:27017/realestate";
+var MongoDB = mongoose.connect(mongoURI).connection;
+
+MongoDB.on("error", function(err) {
+  console.log("Mongo connection error: " + err);
+});
+
+MongoDB.once("open", function() {
+  console.log("Connected to Mongo!");
+});
 
 
 app.set("port", process.env.PORT || 8000);
 
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static("server/public"));
 
 app.get("/", function(req, res) {
   res.sendFile(path.resolve("server/public/views/index.html"));
